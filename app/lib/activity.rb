ef9cf937a1ac1e368963aadf7c1f6aa0477a1a5c
Jeroen Serpieters <jeroen.serpieters@partheas.com>
2013-09-16 13:55:47 +0530

    Only display activities which are not yet fully subscribed

--- app/lib/activity.rb
+++ app/lib/activity.rb
@@ -1,5 +1,14 @@
 class Activity < Sequel::Model
   def self.open_for_subscription(day)
-    where(day: day)
+    DB.transaction do
+      activity_ids = left_outer_join(:subscriptions, activity_id: :id).
+        where("activities.day = ?", day).
+        select_group(:id).
+        having("count(subscriptions.*) < activities.limit").to_a.map do |a|
+          a.id
+      end
+
+      where("id IN ?", activity_ids)
+    end
   end
 end
